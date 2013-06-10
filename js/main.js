72af52d6ddf52c41e5d8c52735d47b347b9cceeb
Jason Lally <jason.lally@gmail.com>
2013-06-10 12:01:13 -0700

    update data loop

--- js/main.js
+++ js/main.js
@@ -28,7 +28,33 @@ function codeAddress() {
                         dataType: "json",
                         url: "http://laiapi-placematters.dotcloud.com/blockgroup/" + blockgroup,
                         success: function(data) {
-                            console.log(data);
+                            var keyIndex;
+                            var div, hh;
+                            var hh_types = { "a" : "Regional Typical",
+                            "b" : "Regional Moderate",
+                            "c" : "Core Typical",
+                            "d" : "Single-Income Family",
+                            "e" : "Low Income",
+                            "f" : "Moderate Income",
+                            "g" : "Single Person Very Low Income",
+                            "h" : "One-Worker Family Very Low Income",
+                            "i" : "Single Professional",
+                            "j" : "Single Worker",
+                            "k" : "Dual-Income Family",
+                            "l" : "Retirees"};
+                            $.each(data, function(k,v) {
+                                keyIndex = k.split("_");
+                                if (keyIndex[0] == 'hh') {
+                                    if(hh != keyIndex[1]) {
+                                        hh = keyIndex[1];
+                                        $('#data').append($('<div class="panel callout"/>').html(hh_types[hh] + " Household Type"));
+                                    }
+                                    div = $('<div class="panel large-12"/>').html(keyIndex[2] + ": " + Math.round(v*100)/100);
+                                } else {
+                                    div = $('<div class="panel large-12"/>').html(k + ": " + v);
+                                }
+                                $('#data').append(div);
+                            });
                         }
                     });
                 }
